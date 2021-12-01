2f4851735f90aa73bff083cec4a25869591e136b
LucaKao <lars10192002@gmail.com>
2021-12-01 20:58:53 +0800

    [2.1]Java for and foreach

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = new int[10];
+        for (int i=0; i<arr.length ;i++)
+            arr[i] = i;
+
+        int[] scores = new int[]{100,22,333,66};
+        for (int i = 0; i < scores.length; i++) {
+//            System.out.println(scores[i]);
+        }
+
+        for (int score:scores) {
+            System.out.println(score);
+        }
+
+        scores[0]=777;
+
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i]);
+        }
+
+
+
+    }
+}
