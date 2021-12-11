30e94c5097e4e5b99575626c906c5c24e94cf0a8
LucaKao <lars10192002@gmail.com>
2021-12-11 20:11:22 +0800

    [3-7] add enquene and dequene func

--- src/com/company/ch3/stack/array/quene/LoopQuene.java
+++ src/com/company/ch3/stack/array/quene/LoopQuene.java
@@ -18,7 +18,7 @@ public class LoopQuene<E> implements Quene<E> {
         this(10);
     }
 
-    public int getCapacity(int capacity){
+    public int getCapacity(){
         return data.length-1;
     }
 
@@ -35,16 +35,80 @@ public class LoopQuene<E> implements Quene<E> {
 
     @Override
     public void enqueue(E e) {
+        if((tail+1) % data.length == front)
+            resize(getCapacity()*2);
+
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size ++;
 
     }
 
     @Override
     public E dequeue() {
-        return null;
+
+        if(isEmpty())
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if(size == getCapacity() / 4 && getCapacity() /2 != 0)
+            resize(getCapacity()/2);
+
+        return ret;
     }
 
     @Override
     public E getFront() {
-        return null;
+
+        if(isEmpty())
+            throw new IllegalArgumentException("Queue is empty.");
+
+        return data[front];
+    }
+
+
+    public void resize(int newCapacity){
+
+        E[] newData =(E[]) new Object[newCapacity+1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+
+        data = newData;
+        front = 0;
+        tail =size;
+    }
+
+    @Override
+    public String toString(){
+
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue: size = %d , capacity = %d\n", size, getCapacity()));
+        res.append("front [");
+        for(int i = front ; i != tail ; i = (i + 1) % data.length){
+            res.append(data[i]);
+            if((i + 1) % data.length != tail)
+                res.append(", ");
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+
+    public static void main(String[] args){
+
+        LoopQuene<Integer> queue = new LoopQuene<>();
+        for(int i = 0 ; i < 10 ; i ++){
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if(i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
     }
 }
