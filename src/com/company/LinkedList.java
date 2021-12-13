d0de7f1adb3fb9aa66055897c38d3e4ea800183b
LucaKao <lars10192002@gmail.com>
2021-12-13 15:15:54 +0800

    [4-3] Add dummyHead to LinkedList

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -30,11 +30,11 @@ public class LinkedList <E> {
 
     }
 
-    private Node head;
+    private Node dummyHead;
     private int size;
 
     public LinkedList(){
-        head = null;
+        dummyHead = null;
         size = 0;
     }
 
@@ -55,8 +55,11 @@ public class LinkedList <E> {
         // head = node ;
 
         //   head = new Node(e, head) => 等於上面三行
-        head = new Node(e, head);
-        size++;
+        //head = new Node(e, head);
+        //size++;
+
+        // use dummuHead
+        add(0,e);
     }
 
 
@@ -66,17 +69,15 @@ public class LinkedList <E> {
         if(index < 0 || index >size)
             throw new IllegalArgumentException("Add faild. Illegal index.");
 
-        if(index == 0)
-            addFirst(e);
-        else{
-            Node prev = head;//得到現在頭部的位置
-            for(int i = 0; i < index -1 ; i++)//查詢要插入的位置前一個的node
+
+            Node prev = dummyHead;//得到現在頭部的位置
+            for(int i = 0; i < index ; i++)//查詢要插入的位置前一個的node
                 prev = prev.next;
 
 
             prev.next = new Node(e, prev.next);
             size ++;
-        }
+
     }
 
     // 在鏈表末尾端添加新的元素e
