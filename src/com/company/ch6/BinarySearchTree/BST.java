7db7a4f374ac501134a2d2406a911f04e8fd0f0e
LucaKao <lars10192002@gmail.com>
2021-12-16 21:06:29 +0800

    [6-3] add BST add element func

--- src/com/company/ch6/BinarySearchTree/BST.java
+++ src/com/company/ch6/BinarySearchTree/BST.java
@@ -30,4 +30,38 @@ public class BST <E extends Comparable<E>>{
     }
 
 
+    // 向二分搜索樹中添加新的元素e
+    public void add(E e){
+        if (root == null){//根節點為空
+            root = new Node(e);//直接在根節點開一個節點
+            size++;
+        }else
+            add(root, e);//從根節點後開始新增節點
+    }
+
+    // 向以node為根的二分搜索樹中插入元素e，遞歸算法
+    private void add(Node node, E e) {
+
+        //遞歸的終止條件
+        if (e.equals(node.e))//插入的元素是否重複(root) 是直接回傳
+            return ;
+        else if (e.compareTo(node.e) > 0 && node.left == null){ // 當插入值(e)大於現有數值(node.e)會回傳正值差距越大越正
+            node.left = new Node(e);
+            size++;
+            return;
+        }else if (e.compareTo(node.e) < 0 && node.right == null){ // 當插入值(e)大於現有數值(node.e)會回傳負值差距越大越負
+
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+
+        if (e.compareTo(node.e) < 0)
+            add(node.left, e);
+        else
+            add(node.right, e);
+
+    }
+
+
 }
